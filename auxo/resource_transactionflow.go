88dcd567029754370a44440d7436914ab7c9a697
Rob Maas <rob.maas@on2it.net>
2023-05-10 21:14:38 +0200

    working draft with terraform plugin framework

--- auxo/resource_transactionflow.go
+++ auxo/resource_transactionflow.go
@@ -2,6 +2,7 @@ package auxo
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
@@ -23,6 +24,13 @@ type transactionflowResourceModel struct {
 	Incoming_protectsurfaces_block []types.String `tfsdk:"incoming_protectsurfaces_block"`
 	Outgoing_protectsurfaces_allow []types.String `tfsdk:"outgoing_protectsurfaces_allow"`
 	Outgoing_protectsurfaces_block []types.String `tfsdk:"outgoing_protectsurfaces_block"`
+}
+
+type flows struct {
+	incomingPSAllow []basetypes.StringValue
+	incomingPSBlock []basetypes.StringValue
+	outgoingPSAllow []basetypes.StringValue
+	outgoingPSBlock []basetypes.StringValue
 }
 
 func NewTransactionflowResource() resource.Resource {
@@ -99,20 +107,16 @@ func (r *transactionflowResource) Create(ctx context.Context, req resource.Creat
 		return
 	}
 
-	for _, flow := range plan.Incoming_protectsurfaces_allow {
-		ps.FlowsFromOtherPS[flow.ValueString()] = zerotrust.Flow{Allow: true}
-	}
+	var f flows
+	f.incomingPSAllow = plan.Incoming_protectsurfaces_allow
+	f.incomingPSBlock = plan.Incoming_protectsurfaces_block
+	f.outgoingPSAllow = plan.Outgoing_protectsurfaces_allow
+	f.outgoingPSBlock = plan.Outgoing_protectsurfaces_block
 
-	for _, flow := range plan.Incoming_protectsurfaces_block {
-		ps.FlowsFromOtherPS[flow.ValueString()] = zerotrust.Flow{Allow: false}
-	}
-
-	for _, flow := range plan.Outgoing_protectsurfaces_allow {
-		ps.FlowsToOtherPS[flow.ValueString()] = zerotrust.Flow{Allow: true}
-	}
-
-	for _, flow := range plan.Outgoing_protectsurfaces_block {
-		ps.FlowsToOtherPS[flow.ValueString()] = zerotrust.Flow{Allow: false}
+	ps, err = setFlowsOnPS(ps, f)
+	if err != nil {
+		resp.Diagnostics.AddError("Error creating transactionflow", err.Error())
+		return
 	}
 
 	ps, err = r.client.ZeroTrust.CreateProtectSurfaceByObject(*ps, true)
@@ -124,32 +128,11 @@ func (r *transactionflowResource) Create(ctx context.Context, req resource.Creat
 
 	// Map resonse to schema
 	plan.Protectsurface = types.StringValue(ps.ID)
-
-	incomingPSAllow := []basetypes.StringValue{}
-	incomingPSBlock := []basetypes.StringValue{}
-	outgoingPSAllow := []basetypes.StringValue{}
-	outgoingPSBlock := []basetypes.StringValue{}
-
-	for psID, flow := range ps.FlowsFromOtherPS {
-		if flow.Allow {
-			incomingPSAllow = append(incomingPSAllow, basetypes.NewStringValue(psID))
-		} else {
-			incomingPSBlock = append(incomingPSBlock, basetypes.NewStringValue(psID))
-		}
-	}
-
-	for psID, flow := range ps.FlowsToOtherPS {
-		if flow.Allow {
-			outgoingPSAllow = append(outgoingPSAllow, basetypes.NewStringValue(psID))
-		} else {
-			outgoingPSBlock = append(outgoingPSBlock, basetypes.NewStringValue(psID))
-		}
-	}
-
-	plan.Incoming_protectsurfaces_allow = incomingPSAllow
-	plan.Incoming_protectsurfaces_block = incomingPSBlock
-	plan.Outgoing_protectsurfaces_allow = outgoingPSAllow
-	plan.Outgoing_protectsurfaces_block = outgoingPSBlock
+	f = readFlowsFromPS(ps)
+	plan.Incoming_protectsurfaces_allow = f.incomingPSAllow
+	plan.Incoming_protectsurfaces_block = f.incomingPSBlock
+	plan.Outgoing_protectsurfaces_allow = f.outgoingPSAllow
+	plan.Outgoing_protectsurfaces_block = f.outgoingPSBlock
 
 	// Set state
 	diags = resp.State.Set(ctx, &plan)
@@ -161,10 +144,175 @@ func (r *transactionflowResource) Create(ctx context.Context, req resource.Creat
 }
 
 func (r *transactionflowResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+	// Get current state
+	var state transactionflowResourceModel
+	diags := req.State.Get(ctx, &state)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// Get refreshed state from AUXO
+	result, err := r.client.ZeroTrust.GetProtectSurfaceByID(state.Protectsurface.ValueString())
+	if err != nil {
+		resp.Diagnostics.AddError("Error reading location", "unexpected error: "+err.Error())
+		return
+	}
+
+	//Overwrite state with refreshed state
+	f := readFlowsFromPS(result)
+	state.Protectsurface = types.StringValue(result.ID)
+	state.Incoming_protectsurfaces_allow = f.incomingPSAllow
+	state.Incoming_protectsurfaces_block = f.incomingPSBlock
+	state.Outgoing_protectsurfaces_allow = f.outgoingPSAllow
+	state.Outgoing_protectsurfaces_block = f.outgoingPSBlock
+
+	//Set refreshed state
+	diags = resp.State.Set(ctx, &state)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
 }
 
 func (r *transactionflowResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+	//Retrieve values from plan
+	var plan transactionflowResourceModel
+
+	diags := req.Plan.Get(ctx, &plan)
+	resp.Diagnostics.Append(diags...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// create transactionflow
+	psID := plan.Protectsurface.ValueString()
+	ps, err := r.client.ZeroTrust.GetProtectSurfaceByID(psID)
+
+	if err != nil {
+		resp.Diagnostics.AddError("Error creating transactionflow", "unexpected error: "+err.Error())
+		return
+	}
+
+	var f flows
+	f.incomingPSAllow = plan.Incoming_protectsurfaces_allow
+	f.incomingPSBlock = plan.Incoming_protectsurfaces_block
+	f.outgoingPSAllow = plan.Outgoing_protectsurfaces_allow
+	f.outgoingPSBlock = plan.Outgoing_protectsurfaces_block
+
+	ps, err = setFlowsOnPS(ps, f)
+	if err != nil {
+		resp.Diagnostics.AddError("Error creating transactionflow", err.Error())
+		return
+	}
+
+	ps, err = r.client.ZeroTrust.CreateProtectSurfaceByObject(*ps, true)
+
+	if err != nil {
+		resp.Diagnostics.AddError("Error creating transactionflow", "unexpected error: "+err.Error())
+		return
+	}
+
+	// Map resonse to schema
+	plan.Protectsurface = types.StringValue(ps.ID)
+	f = readFlowsFromPS(ps)
+	plan.Incoming_protectsurfaces_allow = f.incomingPSAllow
+	plan.Incoming_protectsurfaces_block = f.incomingPSBlock
+	plan.Outgoing_protectsurfaces_allow = f.outgoingPSAllow
+	plan.Outgoing_protectsurfaces_block = f.outgoingPSBlock
+
+	// Set state
+	diags = resp.State.Set(ctx, &plan)
+	resp.Diagnostics.Append(diags...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
 }
 
 func (r *transactionflowResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+	// Retrieve values from state
+	var state transactionflowResourceModel
+	diags := req.State.Get(ctx, &state)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// Get PS and remove flows
+	ps, err := r.client.ZeroTrust.GetProtectSurfaceByID(state.Protectsurface.ValueString())
+	ps.FlowsFromOtherPS = map[string]zerotrust.Flow{}
+	ps.FlowsToOtherPS = map[string]zerotrust.Flow{}
+
+	// Update PS, with deleted flows
+	_, err = r.client.ZeroTrust.CreateProtectSurfaceByObject(*ps, true)
+
+	if err != nil {
+		resp.Diagnostics.AddError("Error deleting state", "unexpected error: "+err.Error())
+		return
+	}
+}
+
+// readFlowsFromPS, get a ProtectSurface and return a flows struct, which can be used to map directly on plan & state
+func readFlowsFromPS(ps *zerotrust.ProtectSurface) flows {
+	var f flows
+
+	for psID, flow := range ps.FlowsFromOtherPS {
+		if flow.Allow {
+			f.incomingPSAllow = append(f.incomingPSAllow, basetypes.NewStringValue(psID))
+		} else {
+			f.incomingPSBlock = append(f.incomingPSBlock, basetypes.NewStringValue(psID))
+		}
+	}
+
+	for psID, flow := range ps.FlowsToOtherPS {
+		if flow.Allow {
+			f.outgoingPSAllow = append(f.outgoingPSAllow, basetypes.NewStringValue(psID))
+		} else {
+			f.outgoingPSBlock = append(f.outgoingPSBlock, basetypes.NewStringValue(psID))
+		}
+	}
+
+	return f
+}
+
+// setFlowsOnPS, get a ProtectSurface and a flows struct, and set the flows on the ProtectSurface
+func setFlowsOnPS(ps *zerotrust.ProtectSurface, flows flows) (*zerotrust.ProtectSurface, error) {
+
+	//Check for duplicates in incoming and outgoing, which is not allowed
+	for _, flow := range flows.incomingPSAllow {
+		if sliceContains(getSliceFromSetOfString(flows.incomingPSBlock), flow.ValueString()) {
+			return nil, fmt.Errorf("duplicate in incoming_protectsurfaces_allow and incoming_protectsurfaces_block, protectsurface ID: %s", flow.ValueString())
+		}
+	}
+
+	for _, flow := range flows.outgoingPSAllow {
+		if sliceContains(getSliceFromSetOfString(flows.outgoingPSBlock), flow.ValueString()) {
+			return nil, fmt.Errorf("duplicate in outgoing_protectsurfaces_allow and outgoing_protectsurfaces_block, protectsurface ID: %s", flow.ValueString())
+		}
+	}
+
+	ps.FlowsFromOtherPS = map[string]zerotrust.Flow{}
+	ps.FlowsToOtherPS = map[string]zerotrust.Flow{}
+
+	for _, flow := range flows.incomingPSAllow {
+		ps.FlowsFromOtherPS[flow.ValueString()] = zerotrust.Flow{Allow: true}
+	}
+
+	for _, flow := range flows.incomingPSBlock {
+		ps.FlowsFromOtherPS[flow.ValueString()] = zerotrust.Flow{Allow: false}
+	}
+
+	for _, flow := range flows.outgoingPSAllow {
+		ps.FlowsToOtherPS[flow.ValueString()] = zerotrust.Flow{Allow: true}
+	}
+
+	for _, flow := range flows.outgoingPSBlock {
+		ps.FlowsToOtherPS[flow.ValueString()] = zerotrust.Flow{Allow: false}
+	}
+
+	return ps, nil
 }
